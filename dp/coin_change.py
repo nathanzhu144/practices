f4d9d4eaa81e88cebc9d7d1eec4544c618dd930e
nathanzhu144 <znathan@umich.edu>
2019-06-13 09:32:34 -0400

    added coin change dp problem

---
+++ dp/coin_change.py
@@ -0,0 +1,42 @@
+#  Nathan Zhu Amex Tower 8:21 am, 36th floor, focus room, Thursday June 13th
+#  Suppose your list of coins is [15, 10, 3, 2, 1]
+#
+#  We have a coin_index that starts at the back.  The idea behind the coin_index is whether to use the 
+#  coin pointed to by coin_index.  
+#
+#                                  [15, 10, 5] amount (20)
+#                  /                                                              \
+#            [15, 10] amount (20)                                         [15, 10, 5]  amount (15)
+#       /             \                                                  /                                 \
+#  [15] amount(20)   [15, 10] amount(10)                     [15, 10] amount (15)               [15, 10, 5]  amount(10)
+#      ...              /     \                                   /        \                           /               \          
+#           [15] amount(10)  [15, 10] amount(0)      [15] amount(15) [15, 10] amount(5)      [15, 10] amount(5)       [15, 10, 5]  amount(5)
+#                ...                                                    ...                         ...                        ...
+#
+#  
+def coinChange(self, coins, amount):
+    """
+    :type coins: List[int]
+    :type amount: int
+    :rtype: int
+    """
+    
+    def helper(coins, amount, coin_index, mem):
+        key = (amount, coin_index)
+        if key in mem:
+            return mem[key]
+        
+        if amount == 0: 
+            return 0
+        
+        if coin_index == -1 or amount < 0:   # 
+            return float('inf')
+        
+        mem[key] = min(helper(coins, amount - coins[coin_index], coin_index, mem) + 1, helper(coins, amount, coin_index - 1, mem))
+        return mem[key]
+    
+    returned = helper(coins, amount, len(coins) - 1, {})
+    if returned == float('inf'):
+        return -1
+    else:
+        return returned
