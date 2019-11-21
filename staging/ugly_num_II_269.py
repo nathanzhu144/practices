8b101110feb51d6412fb9811824a3b30b88b84e3
nathanzhu144 <znathan@umich.edu>
2019-11-21 06:56:49 -0500

    next

--- staging/ugly_num_II_269.py
+++
@@ -1,9 +0,0 @@
-# Write a program to find the n-th ugly number.
-# Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
-
-#  How to find nth, ugly number.
-#
- 
-# 2, 3, 5
-# 4, 
-# 
