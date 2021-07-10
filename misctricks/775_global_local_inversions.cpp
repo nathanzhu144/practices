d8b46c8a25f7930b38925e107e4de7a7e2d83047
Nathan Zhu <znathan@umich.edu>
2021-07-09 19:43:28 -0700

    Added misc tricks 775 global local inversions

---
+++ misctricks/775_global_local_inversions.cpp
@@ -0,0 +1,30 @@
+/* Nathan Zhu
+*  July 9th, 2021.  Friday 7:41 pm.  Fasting today and am hungry.
+*  Leetcode 775 | medium | medium
+*  
+*/
+
+// All local inversions are global inversions.
+// Local & global inversions are same iff there are no global inversions
+// that are not local inversions.  
+
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+
+bool isIdealPermutation(vector<int>& nums) {
+    int maxnum = numeric_limits<int>::min();
+    int N = nums.size();
+    
+    for(int i = 0; i < N; ++i){
+        if(i > 1) { 
+            maxnum = max(maxnum, nums[i - 2]);
+        }
+        
+        if(maxnum > nums[i]) return false;
+    }
+    
+    return true;
+}
